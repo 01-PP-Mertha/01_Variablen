ee768f0e1f3e803bfa6bbefe68f6f617e02eda2c
Mertha89 <mertha@live.de>
2023-05-09 10:22:55 +0200

    3 variablen eingefügt

--- script_01.js
+++ script_01.js
@@ -11,7 +11,20 @@
 // Konsolen-Ausgaben
 // console.log("Hi");
 // console.log(firstName);
+     
 
 
 /***** 02 Deklaration + Wertzuweisung I *******/
+
+let firstName;
+firstName = "Daniel";
+console.log(firstName);
+
+let familyName;
+familyName = "Mertha";
+console.log(familyName);
+
+let birthDate;
+birthDate = "19/04/1989";
+console.log(birthDate);
 
